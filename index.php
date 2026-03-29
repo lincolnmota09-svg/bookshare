74e3b267b2d6830b83736cf7e48e9403a3a2b4b6
Your Name <you@example.com>
2026-03-29 15:44:23 -0300

    atualizar

--- index.php
+++ index.php
@@ -8,8 +8,18 @@ include('./php/verificar.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Início</title>
+    <link rel="stylesheet" href="home.css">
 </head>
 <body>
-    <h1></h1>
+    <header class = "header">
+        <nav class = "nav">
+            <a href="" class="logo"></a>
+            <ul>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Projetos</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
 </body>
 </html>
